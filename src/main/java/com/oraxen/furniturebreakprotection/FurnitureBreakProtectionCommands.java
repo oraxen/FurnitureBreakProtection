b7e2cf8ded9356d6377215f990f8c30c4324c320
Boy <sivertpaulsen2@gmail.com>
2022-11-14 20:12:29 +0100

    update api

--- src/main/java/com/oraxen/furniturebreakprotection/FurnitureBreakProtectionCommands.java
+++ src/main/java/com/oraxen/furniturebreakprotection/FurnitureBreakProtectionCommands.java
@@ -12,8 +12,8 @@ import io.th0rgal.oraxen.shaded.customblockdata.CustomBlockData;
 import io.th0rgal.oraxen.shaded.kyori.adventure.audience.Audience;
 import io.th0rgal.oraxen.shaded.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import io.th0rgal.oraxen.shaded.morepersistentdatatypes.DataType;
+import io.th0rgal.oraxen.utils.AdventureUtils;
 import io.th0rgal.oraxen.utils.BlockHelpers;
-import io.th0rgal.oraxen.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -37,26 +37,26 @@ public class FurnitureBreakProtectionCommands implements CommandExecutor {
         boolean oraxenEnabled = Bukkit.getPluginManager().isPluginEnabled("Oraxen");
         if (sender instanceof Player player && player.isOp() && oraxenEnabled) {
             Audience oraxenAudience = OraxenPlugin.get().getAudience().player(player);
-            TagResolver tags = TagResolver.resolver(TagResolver.standard(), GlyphTag.RESOLVER, ShiftTag.RESOLVER, Utils.tagResolver("prefix", Message.PREFIX.toString()));
+            TagResolver tags = TagResolver.resolver(TagResolver.standard(), GlyphTag.RESOLVER, ShiftTag.RESOLVER, AdventureUtils.tagResolver("prefix", Message.PREFIX.toString()));
             double radius = 20;
             if (args.length != 0) {
                 try {
                     radius = Double.parseDouble(args[0].replace(",", "."));
                     if (radius < 1) {
-                        oraxenAudience.sendMessage(Utils.MINI_MESSAGE.deserialize("<prefix><red>Radius must be greater than 0!", tags));
+                        oraxenAudience.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<prefix><red>Radius must be greater than 0!", tags));
                         return true;
                     }
                 } catch (NumberFormatException ignored) {
-                    oraxenAudience.sendMessage(Utils.MINI_MESSAGE.deserialize("<prefix><red>Radius must be a valid number, " + args[0].replace(",", ".") + " is not.", tags));
+                    oraxenAudience.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<prefix><red>Radius must be a valid number, " + args[0].replace(",", ".") + " is not.", tags));
                     return true;
                 }
             }
-            if (fix(player, radius)) oraxenAudience.sendMessage(Utils.MINI_MESSAGE.deserialize("<prefix><green>Fixed furniture in a radius of " + radius + " blocks.", tags));
-            else oraxenAudience.sendMessage(Utils.MINI_MESSAGE.deserialize("<prefix><red>No furniture found in a radius of " + radius + " blocks.", tags));
+            if (fix(player, radius)) oraxenAudience.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<prefix><green>Fixed furniture in a radius of " + radius + " blocks.", tags));
+            else oraxenAudience.sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<prefix><red>No furniture found in a radius of " + radius + " blocks.", tags));
         } else {
             if (oraxenEnabled) {
-                TagResolver tags = TagResolver.resolver(TagResolver.standard(), GlyphTag.RESOLVER, ShiftTag.RESOLVER, Utils.tagResolver("prefix", Message.PREFIX.toString()));
-                OraxenPlugin.get().getAudience().sender(sender).sendMessage(Utils.MINI_MESSAGE.deserialize("<prefix><red>You must be an OP player to use this command!", tags));
+                TagResolver tags = TagResolver.resolver(TagResolver.standard(), GlyphTag.RESOLVER, ShiftTag.RESOLVER, AdventureUtils.tagResolver("prefix", Message.PREFIX.toString()));
+                OraxenPlugin.get().getAudience().sender(sender).sendMessage(AdventureUtils.MINI_MESSAGE.deserialize("<prefix><red>You must be an OP player to use this command!", tags));
             }
             else sender.sendMessage("Oraxen is not enabled!");
         }
